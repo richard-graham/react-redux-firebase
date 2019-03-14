cca7524518b4bfec42627bec70f89816f8d1e6eb
Richard Graham <richgrahamdev@gmail.com>
2019-03-14 13:08:59 +1300

    added BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'; 
+import { BrowserRouter } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
         <h1>Marioplan</h1>
       </div>
+      </BrowserRouter>
     );
   }
 }
