7220545865e95a66ae6036e821752b1fa79b060b
Richard Graham <richgrahamdev@gmail.com>
2019-03-14 21:10:03 +1300

    end of video 5

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -8,9 +8,10 @@ class Dashboard extends React.Component {
       <div className='dashboard container'>
         <div className='row'>
           <div className='col s12 m6'>
+            <ProjectList />
+          </div>
           <div className='col s12 m5 offset-m1'>
             <Notifications/>
-          </div>
           </div>
         </div>
       </div>
