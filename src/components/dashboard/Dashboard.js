b8943c8d27d9610d72f9c2022f67f1831767726b
Richard Graham <richgrahamdev@gmail.com>
2019-03-14 21:07:45 +1300

    added basic page layout

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,1 +1,21 @@
-  
+import React from 'react'
+import Notifications from './Notifications'
+import ProjectList from '../projects/ProjectList'
+
+class Dashboard extends React.Component {
+  render() {
+    return (
+      <div className='dashboard container'>
+        <div className='row'>
+          <div className='col s12 m6'>
+          <div className='col s12 m5 offset-m1'>
+            <Notifications/>
+          </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Dashboard
