84ccddb32136e576bf1fcf30693ddff1496102a4
Richard Graham <richgrahamdev@gmail.com>
2019-03-15 07:51:51 +1300

    made project summary module

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,33 +1,13 @@
 import React from 'react'
+import ProjectSummary from './ProjectSummary'
 
 const ProjectList = () => {
   return (
     <div className='project-list section'>
-
-      <div className='card z-depth-0 project-summary'>
-        <div className='card-content grey-text text-darken-3'>
-          <span className='card-title'>Project Title</span>
-            <p>Posted By Richard</p>
-            <p className='grey-text'>14th of March, 3pm</p>
-        </div>
-      </div>
-
-      <div className='card z-depth-0 project-summary'>
-        <div className='card-content grey-text text-darken-3'>
-          <span className='card-title'>Project Title</span>
-            <p>Posted By Pete</p>
-            <p className='grey-text'>19th of March, 2pm</p>
-        </div>
-      </div>
-
-      <div className='card z-depth-0 project-summary'>
-        <div className='card-content grey-text text-darken-3'>
-          <span className='card-title'>Project Title</span>
-            <p>Posted By Richard</p>
-            <p className='grey-text'>14th of March, 3pm</p>
-        </div>
-      </div>
-
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
     </div>
   )
 }
---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProjectSummary = () => {
+  return (
+    <div className='card z-depth-0 project-summary'>
+        <div className='card-content grey-text text-darken-3'>
+          <span className='card-title'>Project Title</span>
+            <p>Posted By Richard</p>
+            <p className='grey-text'>14th of March, 3pm</p>
+        </div>
+      </div>
+  )
+}
+
+export default ProjectSummary
