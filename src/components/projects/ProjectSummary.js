20c809144dee184bb6221c5aa3293c1dab226ab9
Richard Graham <richgrahamdev@gmail.com>
2019-03-18 18:58:50 +1300

    connected application to store then outputted to DOM with Project Summary

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const ProjectSummary = (props) => {
+const ProjectSummary = ({project}) => {
   return (
     <div className='card z-depth-0 project-summary'>
         <div className='card-content grey-text text-darken-3'>
-          <span className='card-title'>Project Title</span>
-            <p>Posted By {props.name}</p>
+          <span className='card-title'>{project.title}</span>
+            <p>Posted By Richard Graham</p>
             <p className='grey-text'>14th of March, 3pm</p>
         </div>
       </div>
