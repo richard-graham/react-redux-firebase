f0665349fd0a3c3e344e7b0dfc01af2ac108d2d6
Richard Graham <richgrahamdev@gmail.com>
2019-03-18 18:52:06 +1300

    added reducers and projects redux val

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -2,8 +2,11 @@ import React from 'react'
 import Notifications from './Notifications'
 import ProjectList from '../projects/ProjectList'
 
+import { connect } from 'react-redux'
+
 class Dashboard extends React.Component {
   render() {
+    console.log(this.props);
     return (
       <div className='dashboard container'>
         <div className='row'>
@@ -19,4 +22,10 @@ class Dashboard extends React.Component {
   }
 }
 
-export default Dashboard
+const mapStateToProps = (state) => {
+  return {
+    projects: state.project.projects
+  }
+}
+
+export default connect(mapStateToProps)(Dashboard)
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initialState = {}
+const initialState = {
+  projects: [
+    {id: '1', title: 'Replace broken anchor', content: 'blah blah blah'},
+    {id: '2', title: 'Remove overgrown foliage', content: 'blah blah blah'},
+    {id: '3', title: 'Replace 3rd bolt', content: 'blah blah blah'}
+  ]
+}
 
 const projectReducer = (state = initialState, action) => {
   return state
