1f93fcf200c26843802bf32cb0078a508b3ba6a3
Richard Graham <richgrahamdev@gmail.com>
2019-03-18 17:15:53 +1300

    made create project page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Dashboard from './components/dashboard/Dashboard'
 import ProjectDetails from './components/projects/ProjectDetails'
 import SignIn from './components/auth/SignIn'
 import SignUp from './components/auth/SignUp'
+import CreateProject from './components/projects/CreateProject'
 
 class App extends Component {
   render() {
@@ -17,7 +18,8 @@ class App extends Component {
           <Route exact path='/' component={Dashboard} />
           <Route path='/project/:id' component={ProjectDetails} />
           <Route path='/signin' component={SignIn} />
-          <Route path='signup' component={SignUp} />
+          <Route path='/signup' component={SignUp} />
+          <Route path='/create' component={CreateProject} />
         </Switch>
       </div>
       </BrowserRouter>
