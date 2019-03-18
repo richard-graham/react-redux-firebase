d5de5c8598fe7ebc8ba1c0179c6f5ac4025820e6
Richard Graham <richgrahamdev@gmail.com>
2019-03-18 17:20:08 +1300

    added sign in and sign up links

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -9,6 +9,7 @@ const Navbar = () => {
       <div className="container">
         <Link to="/" className="brand-logo">The Gear Report</Link>
         <SignedInLinks/>
+        <SignedOutLinks />
       </div>
     </nav>
   )
