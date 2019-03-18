a48feab990c890e96c02ab69b7036870ebe7464e
Richard Graham <richgrahamdev@gmail.com>
2019-03-18 22:37:05 +1300

    ran into reference error

--- src/components/projects/CreateProject.js
+++ src/components/projects/CreateProject.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { createProject } from '../../store/actions/projectActions '
 
 class CreateProject extends React.Component{
   state = {
@@ -14,10 +16,12 @@ class CreateProject extends React.Component{
 
   handleSubmit = (e) => {
     e.preventDefault()
-    console.log(this.state);
+    this.props.createProject(this.state)
+
   }
 
   render() {
+    console.log(this.props);
     return (
       <div className='container'>
         <form className='white' onSubmit={this.handleSubmit}>
@@ -43,4 +47,11 @@ class CreateProject extends React.Component{
   }
 }
 
-export default CreateProject
+const mapDispatchToProps = (dispatch) => {
+  // 
+  return {
+    createProject: (project) => dispatch(createProject(project))
+  }
+}
+
+export default connect(null, mapDispatchToProps)(CreateProject)
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -7,6 +7,13 @@ const initialState = {
 }
 
 const projectReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'CREATE_PROJECT':
+    console.log('created project', action.project)
+    break
+    default:
+      return state
+  }
   return state
 }
 
