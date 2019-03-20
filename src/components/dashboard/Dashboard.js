785b8b785f7496d77a945c0af921d8818b62283a
richard-graham <richardcolingraham@gmail.com>
2019-03-20 21:19:58 +1300

    rolled back react-redux to version 5.1.1 to remove 'context' err

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -3,6 +3,8 @@ import Notifications from './Notifications'
 import ProjectList from '../projects/ProjectList'
 
 import { connect } from 'react-redux'
+import { fireStoreConnect, firestoreConnect } from 'react-redux-firebase'
+import { compose } from 'redux'
 
 class Dashboard extends React.Component {
   render() {
@@ -23,9 +25,15 @@ class Dashboard extends React.Component {
 }
 
 const mapStateToProps = (state) => {
+  console.log(state);
   return {
     projects: state.project.projects
   }
 }
 
-export default connect(mapStateToProps)(Dashboard)
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([
+    { collection: 'projects' }
+  ])
+)(Dashboard)
