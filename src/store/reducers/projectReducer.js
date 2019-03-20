20cbb6a0ab3d476fccef324f98d43809e4fae5ae
richard-graham <richgrahamdev@gmail.com>
2019-03-20 15:45:20 +1300

    testing git

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -10,9 +10,9 @@ const projectReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'CREATE_PROJECT':
     console.log('created project', action.project)
-    break
-    default:
-      return state
+    // break
+    // default:
+    //   return state
   }
   return state
 }
