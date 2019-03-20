d5552493eb30fb0d7212d6ca457a657c3a69556a
richard-graham <richardcolingraham@gmail.com>
2019-03-20 21:01:11 +1300

    modified project action, app writing projects to firestore now

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -9,12 +9,14 @@ const initialState = {
 const projectReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'CREATE_PROJECT':
-    console.log('created project', action.project)
-    // break
-    // default:
-    //   return state
+      console.log('created project', action.project)
+      return state
+    case 'CREATE_PROJECT_ERROR':
+      console.log('create project error', action.err);
+      return state
+    default: 
+      return state
   }
-  return state
 }
 
 export default projectReducer
