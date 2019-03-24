4b119c0dc31bdd93d51906a10eecbc609f3c2c26
richard-graham <richardcolingraham@gmail.com>
2019-03-25 11:43:35 +1300

    made new user reducer

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -19,8 +19,20 @@ const authReducer = (state = initialState, action) => {
     case 'SIGNOUT_SUCCESS':
      console.log('signout success');
      return state
-     default:
-      return state
+    case 'SIGNUP_SUCCESS':
+     console.log('signup success');
+     return {
+       ...state,
+       authError: null
+     }
+    case 'SIGNUP_ERROR':
+     console.log('signup error');
+     return {
+       ...state,
+       authError: action.err.message
+     }
+    default:
+    return state
   }
 }
 
