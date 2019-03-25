e29b4c2f389ccf92fe11cf4121621684b2c916ab
richard-graham <richardcolingraham@gmail.com>
2019-03-26 12:19:46 +1300

    deployed to firebase

---
+++ dist/precache-manifest.9f1e886d6eb28f1dc850da1e64891870.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "80998ad7308c24eecd09",
+    "url": "/static/css/main.16e2426e.chunk.css"
+  },
+  {
+    "revision": "80998ad7308c24eecd09",
+    "url": "/static/js/main.80998ad7.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "bf7ea24c742eb030e686",
+    "url": "/static/js/2.bf7ea24c.chunk.js"
+  },
+  {
+    "revision": "842e46a10d2d1e0449e44f9d1ce2b9f6",
+    "url": "/index.html"
+  }
+];
